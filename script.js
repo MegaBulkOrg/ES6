8cfff1e63222097247411eee38edbd82c99f41b5
MegaBulkOrg <sheng.bidebao@gmail.com>
2021-01-14 13:57:56 +0300

    Символы (Symbols)

--- script.js
+++ script.js
@@ -1,41 +1,37 @@
-let persone = {
-	name: 'Pavlick',
-	age: 35,
-	address: {
-		city: 'Saint-Petersburg',
-		index: 197374
-	}
+let symbol = Symbol();
+console.log(typeof symbol);
+
+
+// Имя для символа
+let sym_1 = Symbol('asd');
+console.log(sym_1);
+
+// Для различных символов можно использовать одно и то же имя.
+let sym_2 = Symbol('asd');
+console.log(sym_1 == sym_2);
+
+
+// Создание символов методом for
+let sym_3 = Symbol.for('qaz');
+let sym_4 = Symbol.for('qaz');
+console.log(sym_3 == sym_4);
+
+
+// Метод "keyfor" позволяет получить имя символа
+let symName = Symbol.keyFor(sym_3);
+console.log(symName);
+
+
+// Символы и объекты
+let person = {
+	name: 'Jack',
+	age: 20,
+	[Symbol.for('pass')]: 'qwerty',
+	[Symbol.for('login')]: 'J_1990'
 }
+console.log(person);
+console.log(person[Symbol.for('pass')]);
 
-// Тут используются фигурные скобки
-let {name, age} = persone;
-console.log(name, age);
 
-// Дефолтные значения (справа от знака равенства находится пустой объект)
-let {job='it-web', hobby='pranks'} = {};
-console.log(job, hobby);
-
-// Если имена переменных отличаются от имен свойств объекта (тут синтаксис такой: имяСвойства:имяПеременной и если поменять их местами то будет ошибка)
-let {name:who, age:years} = persone;
-console.log(who, years);
-
-// Если массив многомерный
-let {address:{city, index, street='st. Ho Shi Min'}} = persone;
-console.log(city, index, street);
-
-// Использование деструктивного присваивания объекта в качестве параметра функции
-function getData({name, age}){
-	console.log(name, age);
-}
-getData(persone);
-
-// Использование деструктивного присваивания для возврата нескольких значений из функции
-function getInfo({name, age}){
-	return {name, age}
-}
-// -- из функции у нас вернулся объект
-let info = getInfo(persone);
-console.log(info);
-// -- значение свойств объекта присвоим в переменные
-let {name:mingzi, age:old} = getInfo(persone);
-console.log(mingzi, old);
+// Метод "getOwnPropertySymbols" позволяет просмотреть все символы у объекта
+console.log(Object.getOwnPropertySymbols(person));
