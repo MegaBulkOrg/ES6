65a4a8a0d50b17a17cc766ec2c585e2778d3fb7c
MegaBulkOrg <sheng.bidebao@gmail.com>
2021-01-14 16:34:19 +0300

    Промисы или обещания (Promise)

--- script.js
+++ script.js
@@ -1,37 +1,23 @@
-let symbol = Symbol();
-console.log(typeof symbol);
+// Абстрактный пример
+const prom = new Promise((resolve, reject) => {
+	throw new Error('что-то пошло не так...');	
+	setTimeout(_ => {
+		if (true)
+		{resolve('promise completed')}
+		else
+		{reject('')}
+	}, 3000)
+});
+prom
+	.then(data => console.log(data))
+	.catch(error => console.log(error));
 
 
-// Имя для символа
-let sym_1 = Symbol('asd');
-console.log(sym_1);
-
-// Для различных символов можно использовать одно и то же имя.
-let sym_2 = Symbol('asd');
-console.log(sym_1 == sym_2);
-
-
-// Создание символов методом for
-let sym_3 = Symbol.for('qaz');
-let sym_4 = Symbol.for('qaz');
-console.log(sym_3 == sym_4);
-
-
-// Метод "keyfor" позволяет получить имя символа
-let symName = Symbol.keyFor(sym_3);
-console.log(symName);
-
-
-// Символы и объекты
-let person = {
-	name: 'Jack',
-	age: 20,
-	[Symbol.for('pass')]: 'qwerty',
-	[Symbol.for('login')]: 'J_1990'
-}
-console.log(person);
-console.log(person[Symbol.for('pass')]);
-
-
-// Метод "getOwnPropertySymbols" позволяет просмотреть все символы у объекта
-console.log(Object.getOwnPropertySymbols(person));
+// Конкретный пример
+fetch('https//jsonplaceholder.typicode.com/posts/')
+	// получаем от сервера данные и применяем к ним метод json
+	.then(res => res.json())
+	// далее преобразованные данные выводим в консоль
+	.then(data => console.log(data))
+	// если сервер вернет ошибку она отлавливается в catch 
+	.catch(_ => console.log('что-то пошло не так...'));
