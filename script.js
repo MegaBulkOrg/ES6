d9990c201f19bc026e4752ecd9db374d3eed3db9
MegaBulkOrg <sheng.bidebao@gmail.com>
2021-01-15 18:28:18 +0300

    Set & Map

--- script.js
+++ script.js
@@ -1,54 +1,64 @@
-function *gen(){
-	console.log('Шаг № 1');
-	console.log('Шаг № 2');
-	// ключевое слово "yield" возвращает промежуточные данные функции-генератора (объект со свойствами value и done) и отдает контроль над функцией
-	yield 're-re ;)';
-	yield 'ку-ку ;)';
-	console.log('Шаг № 3');
-	console.log('Шаг № 4');
-	console.log('Шаг № 5');
-}
-let iterator = gen();
-// методом "next" мы запустили выполнение функции "gen", после чего выполнение дошло до ключевого слова "yield", вернуло результат и остановилось
-console.log(iterator.next());
-// вызов метода "next" второй раз продолжило выполнение функции "gen"
-console.log(iterator.next());
+// Коллекция MAP
 
+// используем экземпляр класса "map"
+let coll_map = new Map();
 
-function *example_1(){
-	console.log('Шаг № 1');
-	// здесь ключевое слово "yield" в скобках так как без этого будет ошибка
-	let res = (yield) * 5;
-	console.log('Результат равен ' + res);
-}
-let ex_iterator_1 = example_1();
-//console.log(ex_iterator.next(8));
-//console.log(ex_iterator.next(5));
-ex_iterator_1.next();
-// значение указанное в качестве аргумента функции "next" подставится вместо "yield"
-ex_iterator_1.next(2);
-
-
-function *example_2(){
-	try {
-		yield 100;
-		yield 200;
-		yield 300;	
-	} catch (err) {
-		console.log('Атас! '+err);
-	}
-}
-let ex_iterator_2 = example_2();
-console.log(ex_iterator_2.next());
+// добавляем в коллекцию элементы которым соответствуют ключи записанные в трех различных вариантах: строка, число и булево 
+// полный синтаксис
 /*
-при помощи метода return выполнение генератора остановлено на втором шаге то есть свойство "done" стало равным "true"
-строка с использованием метода return закомментирована чтобы не мешать тестированию метода throw
+coll_map.set('строка', 'string');
+coll_map.set(25, 'number');
+coll_map.set(true, 'boolean');
 */
-// console.log(ex_iterator_2.return());
-/*
-при помощи метода throw можно получить ошибку и если в этот метод отправить значение (например создание ошибки)
-то созданную нами ошибку можно увидеть в консоли НО не на вкладке с сообщениями а на вкладке с ошибками
-чтобы ее отловить и переместить на вкладку с сообщениями нужно воспользоваться конструкцией try...catch 
-*/
-console.log(ex_iterator_2.throw(new Error('Что-то пошло не так')));
-console.log(ex_iterator_2.next());
+// сокращенный синтаксис
+coll_map
+	.set('строка', 'string')
+	.set(25, 'number')
+	.set(true, 'boolean');
+
+// при выводе элементов в "get" передается не индекс элемента а его необычный ключ
+console.log(coll_map.get(25));
+console.log(coll_map.get(true));
+
+// метод "size" хранит общее количество записей в коллекции
+console.log(coll_map.size);
+
+// создание коллекции без метода "set" - при таком подходе к созданию MAP главное чтобы аргументом new Map был итерируемый объект
+let new_map = new Map([
+	['text', 'string'],
+	[52, 'number'],
+	[false, 'станция Бубулёво'],
+	[158, 'это сумма']
+])
+console.log(new_map.get(false));
+
+// метод "has" позволяет проверить наличен=е в коллекции какого-либо ключа; он вернет true или false
+console.log(new_map.has(52));
+
+// Удаление элементов в коллекции MAP
+console.log(new_map.size);
+// метод "delete" удаляет запись с переданным аргументом или ключом; он возвращает true если такая запись была найдена, иначе вернется false
+console.log(new_map.delete(158));
+console.log(new_map.size);
+// метод "clear" удаляет все записи
+console.log(new_map.clear());
+console.log(new_map.size);
+
+
+
+
+// Коллекция SET - используется для хранения множества значений; при этом каждое значение уникальное
+let user_1 = {name: 'тов. Максим'};
+let user_2 = {name: 'Рудой'};
+let user_3 = {name: 'Костян'};
+let users = new Set();
+users
+	.add(user_1)
+	.add(user_2)
+	.add(user_3)
+	.add(user_1)
+	.add(user_1)
+// Хоть первый элемент добавлен 3 раза результат будет не 6 а 3
+console.log(users.size);
+// Метод "forEach" может применяться и к коллекциям MAP
+users.forEach(user => console.log(user))
