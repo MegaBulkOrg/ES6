ab073d4310663ea1b30ad21eb5bc12789df67bb2
MegaBulkOrg <sheng.bidebao@gmail.com>
2021-01-19 18:29:21 +0300

    Proxy

--- script.js
+++ script.js
@@ -1,47 +1,47 @@
-class Members {
-	constructor(name){
-		this.name = name
-	}
-	greet(){
-		console.log(`Hello, ${this.name} !`);
-	}
-}
-class MembersInfo {
-	boobs = '3+'
-}
-
-
-// Создание экземпляра класса
-const Myra = Reflect.construct(Members, ['Мира'], MembersInfo);
-// Из-за третьего параметра созданный объект является не объектом класса Members а объектом класса MembersInfo
-console.log(Myra);
-// Вот тут получим false
-console.log(Myra.__proto__ == Members.prototype);
-// Вот тут получим true
-console.log(Myra.__proto__ == MembersInfo.prototype);
-
-
-class Books {
-	constructor(name){
-		this.name = name
-	}
-	greet(){
-		console.log(`Я купил книгу ${this.name}`);
+const valid = {
+	get(target, prop) {
+		return prop in target ? target[prop] : `Поле ${prop} в объекте нет`
+	},
+	set(target, prop, value) {
+		if(value.length > 2){
+			Reflect.set(target, prop, value)
+		} else {
+			console.log('Длинная должна быть более 2 символов')
+		}
 	}
 }
 
 
-const Murakami = Reflect.construct(Books, ['Хроники заводной птицы']);
-// Метод Reflect.apply позволяет вызывать какие либо методы с нужным контекстом и с нужными параметрами
-Reflect.apply(Murakami.greet, {name: 'Норвежский лес'}, [])
-// Метод Reflect.ownKeys позволяет увидеть собственные ключи объекта (ключи методов не отображается так как он попадает в прототип)
-console.log(Reflect.ownKeys(Murakami));
-// Вот так можно добавить новое свойство
-Murakami.price = '500 рублей';
-console.log(Murakami);
-// Вот так можно заблокировать добавление новых свойств
-Reflect.preventExtensions(Murakami);
-Murakami.shop = 'в ТРК Бакланский';
-console.log(Murakami);
-// Вот так можно узнать заблокирован ли объект (если да то результат будет false)
-console.log(Reflect.isExtensible(Murakami));
+// Создаем объект
+const form = {
+	login: 'Poul',
+	pass: '12345'
+}
+
+// Создаем прокси объект
+const formProxy = new Proxy(form, valid);
+// Смотрим что получилось
+console.log(formProxy);
+// Получаем доступ к свойству объекта
+console.log(formProxy.login);
+// Получаем доступ к несуществующему свойству объект (работает геттер)
+console.log(formProxy['username']);
+// Изменяем значение свойства (работает сеттер)
+formProxy.pass = 'i';
+console.log(formProxy);
+
+
+function alert(txt){
+	console.log('[msg]: ', txt)
+}
+const formAlert = new Proxy(alert,{
+	apply (target, thisArg, argArray){
+		if (argArray.length == 1){
+			Reflect.apply(target, thisArg, argArray)
+		} else {
+			console.log('Количество аргументов не совпадает');
+		}
+	}
+});
+formAlert('Рубята, атас!');
+formAlert();
