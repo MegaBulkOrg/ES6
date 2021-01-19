e05bc14871799f01647a422819f90e9c3de52003
MegaBulkOrg <sheng.bidebao@gmail.com>
2021-01-19 17:55:37 +0300

    Reflect

--- script.js
+++ script.js
@@ -1,21 +1,47 @@
-import {txt, first, second, Person, sayHello, str} from "./module.js";
+class Members {
+	constructor(name){
+		this.name = name
+	}
+	greet(){
+		console.log(`Hello, ${this.name} !`);
+	}
+}
+class MembersInfo {
+	boobs = '3+'
+}
 
-console.log(txt, first, second, str);
 
-let PersonName = new Person('Стася');
-console.log(PersonName.name);
+// Создание экземпляра класса
+const Myra = Reflect.construct(Members, ['Мира'], MembersInfo);
+// Из-за третьего параметра созданный объект является не объектом класса Members а объектом класса MembersInfo
+console.log(Myra);
+// Вот тут получим false
+console.log(Myra.__proto__ == Members.prototype);
+// Вот тут получим true
+console.log(Myra.__proto__ == MembersInfo.prototype);
 
-sayHello();
 
-// При дефолтном импорте скобки у названия того что импортируется не нужны
-import Now from "./module.js";
-Now();
+class Books {
+	constructor(name){
+		this.name = name
+	}
+	greet(){
+		console.log(`Я купил книгу ${this.name}`);
+	}
+}
 
-/*
-//Импорт всего что экспортируется из файла ('all' это не ключевое слово а просто переменная)
-import * as all from "./module.js";
-console.log(all.txt, all.first, all.second, all.str);
-let PersonName = new all.Person('Стася');
-console.log(PersonName.name);
-all.sayHello();
-*/
+
+const Murakami = Reflect.construct(Books, ['Хроники заводной птицы']);
+// Метод Reflect.apply позволяет вызывать какие либо методы с нужным контекстом и с нужными параметрами
+Reflect.apply(Murakami.greet, {name: 'Норвежский лес'}, [])
+// Метод Reflect.ownKeys позволяет увидеть собственные ключи объекта (ключи методов не отображается так как он попадает в прототип)
+console.log(Reflect.ownKeys(Murakami));
+// Вот так можно добавить новое свойство
+Murakami.price = '500 рублей';
+console.log(Murakami);
+// Вот так можно заблокировать добавление новых свойств
+Reflect.preventExtensions(Murakami);
+Murakami.shop = 'в ТРК Бакланский';
+console.log(Murakami);
+// Вот так можно узнать заблокирован ли объект (если да то результат будет false)
+console.log(Reflect.isExtensible(Murakami));
